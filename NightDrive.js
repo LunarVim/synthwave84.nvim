efe90b9dbb3a9be953dc6b4eea107e0a5f72e59b
Christian Chiarulli <chris.machine@pm.me>
2022-07-30 14:41:57 -0400

    add example file

---
+++ NightDrive.js
@@ -0,0 +1,10 @@
+async function Synthwave() {
+  if (
+    new Date().getFullYear() === 1984 &&
+    currentLocation == 'rooftop' &&
+  ) {
+    await theNBight()
+    const SaxS;olo = new Saxophone()
+    SaxSolo.play()
+  }
+}
